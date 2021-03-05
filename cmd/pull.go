6c1f12ffb60cd1f81171ce8f00c5a4ef108f5e4c
Peter Koomen <koomen@gmail.com>
2021-03-05 08:25:33 -0800

    Add pull command

--- cmd/pull.go
+++ cmd/pull.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
 
 	"github.com/koomen/eulercli/util"
@@ -21,24 +20,28 @@ var pullCmd = &cobra.Command{
 	Long:  fmt.Sprintf("Download solution templates to ./templates"),
 	Run: func(cmd *cobra.Command, args []string) {
 		util.CreateTempDir()
-		// defer util.RemoveTempDir()
+		defer util.RemoveTempDir()
 
 		owner, repo, branch := "koomen", "eulercli", "main"
+
+		fmt.Printf("Downloading templates from https://github.com/%s/%s\n", owner, repo)
 
 		zippedRepo := util.TempPath(fmt.Sprintf("%s-%s.zip", repo, branch))
 		err := util.DownloadRepo(owner, repo, branch, zippedRepo)
 		cobra.CheckErr(err)
 
 		// Unzip the repository
-		unzipCmd := exec.Command("unzip", "-o", zippedRepo, "-d", util.TempPath(""))
-		//unzipCmd.Stdout = os.Stdout
-		err = unzipCmd.Run()
+		fmt.Printf("Unzipping %s\n", zippedRepo)
+		err = util.Unzip(zippedRepo, util.TempPath(""))
 		cobra.CheckErr(err)
 
+		// Sync the templates directory to the working directory
 		unzippedRepo := util.TempPath(fmt.Sprintf("%s-%s", repo, branch))
 		tmplDir := filepath.Join(unzippedRepo, "templates")
-
-		err = util.SyncDirs(tmplDir, "./templates", false, os.Stdin)
+		dst := "./templates"
+		fmt.Printf("Syncing %s to ./templates\n", tmplDir)
+		err = util.SyncDirs(tmplDir, dst, false, os.Stdin)
 		cobra.CheckErr(err)
+		fmt.Printf("Successfully pulled template solution files to ./templates\n")
 	},
 }
